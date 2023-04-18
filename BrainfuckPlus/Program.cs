b51198f410ee9af7fc4b1189b1f595eff3d55266
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-04-18 23:30:47 +0100

    Added comements planning arguements for the command line

--- BrainfuckPlus/Program.cs
+++ BrainfuckPlus/Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Options
+             * 
+             * debug - transpile, run with debug characters enabled
+             * transpile - transpile and output as a file
+             * run - transpile, run without debug characters enabled
+             * ? - display help
+             * 
+             * the file path should follow the option
+             * if the file path is after the main command and their is no option (like if the file was opened) do te equivalent of run
+             * 
+            */
+
             string? fileAddress = GetFileAddress();
-            GetBFSourceCode.GetCode(fileAddress);
+            GetBFSourceCode.GetCode(fileAddress, true);
         }
 
         static string? GetFileAddress()
