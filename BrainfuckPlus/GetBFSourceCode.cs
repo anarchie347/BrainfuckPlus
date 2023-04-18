dbe6541119576e9a9ed414c10534ea1e36f976ba
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-04-18 08:49:55 +0100

    Added code to get all available methods from directory

---
+++ BrainfuckPlus/GetBFSourceCode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainfuckPlus
+{
+    internal static class GetBFSourceCode
+    {
+        public static void GetCode(string? address)
+        {
+            string RawText = (address == null ? InputCode.Input() : File.ReadAllText(address));
+            string allowedCharSet = "+-,.<>[]";
+            allowedCharSet += (address == null ? "" : GetAvailableMethodNames(address));
+            Console.WriteLine(allowedCharSet);
+        }
+
+        public static string GetAvailableMethodNames(string address)
+        {
+            address = address.Substring(0, address.LastIndexOf('\\'));
+            string[] fileNames;
+            fileNames = Directory.GetFiles(address);
+            fileNames = fileNames.Where(name => name.EndsWith(".bfp")).ToArray();
+            fileNames = fileNames.Select(name => name[..^4]).ToArray(); //remove file extension
+            fileNames = fileNames.Select(name => name[(name.LastIndexOf('\\') + 1)..]).ToArray(); //remove path to just leave filename
+            string fileFirstChars = "";
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                Console.WriteLine(fileNames[i]);
+                fileFirstChars += fileNames[i][0];
+            }
+            return fileFirstChars;
+        }
+
+
+        
+    }
+
+}
--- BrainfuckPlus/Program.cs
+++ BrainfuckPlus/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string? fileAddress = GetFileAddress();
+            GetBFSourceCode.GetCode(fileAddress);
+        }
+
+        static string? GetFileAddress()
+        {
+            string? FileAddress;
+            do
+            {
+                Console.WriteLine("Enter File address");
+                FileAddress = Console.ReadLine() ?? "";
+            } while (FileAddress.Length == 0 || !File.Exists(FileAddress));
+            return FileAddress.Length > 0 ? FileAddress.Replace('/', '\\') : null;
+            
+
         }
     }
 }
