203c154e4b9290a90423473dafa25be8f12d5a41
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-04-20 19:32:17 +0100

    Began work on BFP to BF conversion

--- BrainfuckPlus/ConvertToBF.cs
+++ BrainfuckPlus/ConvertToBF.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,29 @@ namespace BrainfuckPlus
 {
     internal static class ConvertToBF
     {
-        public static void Convert(string code)
+        public static string Convert(string code, string extraValidChars,string fileAddress)
         {
-            throw new NotImplementedException();
+            string directory = Path.GetDirectoryName(fileAddress) ?? fileAddress;
+            code = RecursiveFindSubstitutions(code, extraValidChars, directory);
+
+            return code;
+        }
+
+        public static string RecursiveFindSubstitutions(string code, string extraValidChars, string directory)
+        {
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (extraValidChars.Contains(code[i]))
+                {
+                    code = Substitute(code, code[i], directory);
+                }
+            }
+        }
+
+        public static string Substitute(string code, int charIndex, string directory)
+        {
+            string codeToInsert = File.ReadAllText($"{directory}/{charIndex}.{Program.FILE_EXTENSION}");
+            code = code.Substring(0,code)
         }
     }
 }
