20c73b31436e7592388f8ccff4b8d5bc4bb21430
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-05-11 23:11:32 +0100

    Moved error messages class to its own file Also renamed it to 'Errors' and corrected a typo in the namespace (BrainFuckPlus -> BrainfuckPlus)

--- BrainfuckPlus/BFInterpreter.cs
+++ BrainfuckPlus/BFInterpreter.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Threading;
 
-namespace BrainFuckPlus
+namespace BrainfuckPlus
 {
     internal class BFInterpreter
     {
@@ -18,7 +18,6 @@ namespace BrainFuckPlus
 
             //given memory number = number of bits
             byte[] Memory = new byte[30000];
-
             
             int currentMemoryPointerPosition = 0;
             int interpreterPosition = 0;
@@ -45,7 +44,7 @@ namespace BrainFuckPlus
                         if (currentMemoryPointerPosition == 0)
                         {
 
-                            ErrorMessages.NegativePointerPosition(interpreterPosition);
+                            Errors.NegativePointerPosition(interpreterPosition);
                         }
                         else
                         {
@@ -56,7 +55,7 @@ namespace BrainFuckPlus
                     case '>':
                         if (currentMemoryPointerPosition == Memory.Length - 1)
                         {
-                            ErrorMessages.PointerOverflow(Memory.Length, interpreterPosition);
+                            Errors.PointerOverflow(Memory.Length, interpreterPosition);
                         }
                         else
                         {
@@ -84,7 +83,7 @@ namespace BrainFuckPlus
                         }
                         catch
                         {
-                            ErrorMessages.NoCorrespondingChar(Memory[currentMemoryPointerPosition], currentMemoryPointerPosition, interpreterPosition);
+                            Errors.NoCorrespondingChar(Memory[currentMemoryPointerPosition], currentMemoryPointerPosition, interpreterPosition);
                         }
                         break;
                     case ',':
@@ -104,7 +103,7 @@ namespace BrainFuckPlus
                         }
                         catch
                         {
-                            ErrorMessages.InvalidCharEntered(keyInput, interpreterPosition);
+                            Errors.InvalidCharEntered(keyInput, interpreterPosition);
                         }
                         break;
                     case '[':
@@ -127,7 +126,7 @@ namespace BrainFuckPlus
                                     }
                                     if (searchForCloseBracketIndex == code.Length)
                                     {
-                                        ErrorMessages.NoCorrespondinCloseBracket(interpreterPosition);
+                                        Errors.NoCorrespondinCloseBracket(interpreterPosition);
                                     }
                                     searchForCloseBracketIndex++;
                                 }
@@ -170,7 +169,7 @@ namespace BrainFuckPlus
 
                                     if (searchForOpenBracketIndex == -1)
                                     {
-                                        ErrorMessages.NoCorrespondingOpenBracket(interpreterPosition);
+                                        Errors.NoCorrespondingOpenBracket(interpreterPosition);
                                     }
                                     searchForOpenBracketIndex--;
                                 }
@@ -255,103 +254,6 @@ namespace BrainFuckPlus
                     return true;
             }
             return false;
-        }
-    }
-
-    public static class ErrorMessages
-    {
-        public static void InvalidMemory(string errorInfo)
-        {
-            StartOfErrorMessage(-1);
-
-            Console.WriteLine("Error: Invalid memory setting");
-            Console.WriteLine("Memory setting must be a postive integer value and must be the only thing on the first line of the program");
-            Console.WriteLine("The memory number represents the amount of memory cells required");
-            Console.WriteLine("'" + errorInfo + "' is not a positive integer");
-            EndOfErrorMessage();
-        }
-
-        public static void NegativePointerPosition(int interpreterPointerPosition)
-        {
-            StartOfErrorMessage(interpreterPointerPosition);
-            Console.WriteLine("Error: Negative pointer position");
-            Console.WriteLine("The position of the pointer must be >= 0");
-            EndOfErrorMessage();
-        }
-        public static void PointerOverflow(int maxMemory, int interpreterPointerPosition)
-        {
-            StartOfErrorMessage(interpreterPointerPosition);
-            Console.WriteLine("Error: pointer position must be less than the amount of available memory cells");
-            Console.WriteLine("The maximum index of a memory cell is " + (maxMemory - 1) + " and the programme is trying to acces memory cell " + (maxMemory));
-            EndOfErrorMessage();
-        }
-
-        //Interpreter now uses Byte data type (like most brainfuck programs) so the number wraps around and there is no overflow
-        //public static void IntegerOverflow(int memoryPointerPostion , int interpreterPointerPosition)
-        //{
-        //    StartOfErrorMessage(interpreterPointerPosition);
-        //    Console.WriteLine("Error: Integer overflow");
-        //    Console.WriteLine("The maximum value for an integer is 2,147,483,647, and memory cell " + memoryPointerPostion + " exceeded this value");
-        //    EndOfErrorMessage();
-        //}
-
-        //public static void IntegerUnderflow(int memoryPointerPostion, int interpreterPointerPosition)
-        //{
-        //    StartOfErrorMessage(interpreterPointerPosition);
-        //    Console.WriteLine("Error: Integer underflow");
-        //    Console.WriteLine("The minimum value for an integer is -2,147,483,648, and memory cell " + memoryPointerPostion + " was below this value");
-        //    EndOfErrorMessage();
-        //}
-
-        public static void NoCorrespondingChar(int value, int memoryPointerPosition, int interpreterPointerPosition)
-        {
-            StartOfErrorMessage(interpreterPointerPosition);
-            Console.WriteLine("Error: integer value cannot be converted to a unicode character");
-            Console.WriteLine($"{value} in memory position {memoryPointerPosition} does not have a corresponding character");
-            EndOfErrorMessage();
-        }
-
-        public static void InvalidCharEntered(char value, int interpreterPointerPosition)
-        {
-            StartOfErrorMessage(interpreterPointerPosition);
-            Console.WriteLine("Error: invalid character entered by user");
-            Console.WriteLine($"The user entered {value}, but this cannot be converted to an integer");
-            EndOfErrorMessage();
-        }
-
-        public static void NoCorrespondingOpenBracket(int interpreterPointerPosition)
-        {
-            StartOfErrorMessage(interpreterPointerPosition);
-            Console.WriteLine("Error: no corresponding [ was found");
-            Console.WriteLine($"The ] at position {interpreterPointerPosition} did not have a corresponding [");
-            EndOfErrorMessage();
-        }
-
-        public static void NoCorrespondinCloseBracket(int interpreterPointerPosition)
-        {
-            StartOfErrorMessage(interpreterPointerPosition);
-            Console.WriteLine("Error: no corresponding ] was found");
-            Console.WriteLine($"The [ at position {interpreterPointerPosition} did not have a corresponding ]");
-            EndOfErrorMessage();
-        }
-
-
-        private static void StartOfErrorMessage(int interPreterPointerPosition)
-        {
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Red;
-            //Console.Clear();
-            Console.WriteLine();
-            Console.WriteLine("Encountered an error at character " + interPreterPointerPosition.ToString());
-
-        }
-
-        private static void EndOfErrorMessage()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-            Environment.Exit(0);
         }
     }
 }
