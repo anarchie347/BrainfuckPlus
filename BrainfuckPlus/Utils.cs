a642622fe9c84cd2cafeecc7e099a98b4d9d6c55
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-05-17 22:03:32 +0100

    Started work on a method to output as a file

---
+++ BrainfuckPlus/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BrainfuckPlus
+{
+    internal static class Utils
+    {
+        public static void WriteToFile(string address, string value)
+        {
+            StreamWriter sw;
+            if (!File.Exists(address))
+            {
+                sw = new StreamWriter(address);
+                sw.Write(value);
+                sw.Close();
+                return;
+            }
+            while (File.Exists(address) || address == string.Empty)
+            {
+                Console.WriteLine($"The file {address} already exists");
+                Console.WriteLine("Please enter a new filename/path or leave blank to overwrite: ");
+                address = Console.ReadLine() ?? string.Empty;
+            }
+
+        }
+    }
+}
