955171fa8108bb491ee9182b850a1b465c6773e7
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-05-22 19:51:00 +0100

    Restructuring

--- BrainfuckPlus/Export.cs
+++ BrainfuckPlus/Export.cs
@@ -13,7 +13,7 @@ namespace BrainfuckPlus
     {
         public static void CreateCompressedFile(string address, bool removeComments, string? outputName = null)
         {
-            string[] paths = GetFileAddresses(address);
+            string[] paths = Utils.GetFileAddresses(address);
             string currentDateTime = DateTime.Now.ToFileDateFormat();
             string tempDir = Path.Combine(Path.GetDirectoryName(address),$"bfcompressiontemp_{currentDateTime}");
             string outputPath;
@@ -49,44 +49,7 @@ namespace BrainfuckPlus
                 Directory.Delete(tempDir, true);
             }
         }
-        private static string[] GetFileAddresses(string address)
-        {
-            List<string> paths = RecursiveGetFiles(address);
-            List<string> pathsNoDuplicates = new();
-            pathsNoDuplicates.Add(address);
-            for (int i = 0; i < paths.Count; i++)
-            {
-                if (!pathsNoDuplicates.Contains(paths[i]))
-                    pathsNoDuplicates.Add(paths[i]);
-            }
-            return pathsNoDuplicates.ToArray();
-        }
-        private static List<string> RecursiveGetFiles(string address)
-        {
-            List<string> paths = new();
-            string? methodpath;
-            string[] code = File.ReadAllText(address).Split(Environment.NewLine).Select(x => x.Trim()).ToArray();
-            for (int i = 0; i < code.Length; i++)
-            {
-                for (int j = 0; j < code[i].Length; j++)
-                {
-                    if (code[i][j] == Syntax.COMMENT_CHAR)
-                        break;
-                    if (!(Syntax.EXTRA_ALLOWED_CHARS.Contains(code[i][j]) || Syntax.BF_VALID_CHARS.Contains(code[i][j]) || char.IsWhiteSpace(code[i][j])))
-                    {
-                        methodpath = ConvertToBF.GetAddress(code[i][j], Path.GetDirectoryName(address));
-                        if (methodpath != null && !paths.Contains(methodpath))
-                        {
-                            paths.Add(methodpath);
-                            paths.AddRange(RecursiveGetFiles(methodpath));
-                        }
-
-                    }
-                }
-            }
-
-            return paths;
-        }
+        
 
         private static void RemoveChars(string[] paths)
         {
--- BrainfuckPlus/Utils.cs
+++ BrainfuckPlus/Utils.cs
@@ -84,5 +84,45 @@ namespace BrainfuckPlus
             }
             return code;
         }
+
+
+        public static string[] GetFileAddresses(string address)
+        {
+            List<string> paths = RecursiveGetFiles(address);
+            List<string> pathsNoDuplicates = new();
+            pathsNoDuplicates.Add(address);
+            for (int i = 0; i < paths.Count; i++)
+            {
+                if (!pathsNoDuplicates.Contains(paths[i]))
+                    pathsNoDuplicates.Add(paths[i]);
+            }
+            return pathsNoDuplicates.ToArray();
+        }
+        private static List<string> RecursiveGetFiles(string address)
+        {
+            List<string> paths = new();
+            string? methodpath;
+            string[] code = File.ReadAllText(address).Split(Environment.NewLine).Select(x => x.Trim()).ToArray();
+            for (int i = 0; i < code.Length; i++)
+            {
+                for (int j = 0; j < code[i].Length; j++)
+                {
+                    if (code[i][j] == Syntax.COMMENT_CHAR)
+                        break;
+                    if (!(Syntax.EXTRA_ALLOWED_CHARS.Contains(code[i][j]) || Syntax.BF_VALID_CHARS.Contains(code[i][j]) || char.IsWhiteSpace(code[i][j])))
+                    {
+                        methodpath = ConvertToBF.GetAddress(code[i][j], Path.GetDirectoryName(address));
+                        if (methodpath != null && !paths.Contains(methodpath))
+                        {
+                            paths.Add(methodpath);
+                            paths.AddRange(RecursiveGetFiles(methodpath));
+                        }
+
+                    }
+                }
+            }
+
+            return paths;
+        }
     }
 }
